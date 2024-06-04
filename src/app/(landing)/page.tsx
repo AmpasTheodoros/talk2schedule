4ea1500c5a96a4a70e7cfae698ec343e81202171
Theodor <71594408+AmpasTheodoros@users.noreply.github.com>
2024-06-04 23:37:52 +0300

    Clerk webhook and small fixes

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,97 +1,24 @@
-"use client";
+import Navbar from "@/components/global/navbar";
+import { BackgroundGradientAnimation } from "@/components/aceternity/background-gradient-animation"
+import { FlipWords } from "@/components/aceternity/flip-words";
+import Conversation from "@/components/main/Conversation/ConversationComponent"
 
-import * as z from "zod";
-import axios from "axios";
-import { MessageSquare } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import OpenAI from "openai";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { cn } from "@/lib/utils";
-import { formSchema } from "./constants";
-import MicrophoneComponent from "@/components/MicrophoneComponent";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { DM_Serif_Display } from "next/font/google";
-import { Button } from "@/components/ui/button";
+const Home = () => {
+    const words = ["Sync", "Schedule", "Organise", "Add Tasks"];
 
-
-
-const HomePage = () => {
-  const router = useRouter();
-  const [messages, setMessages] = useState<OpenAI.Chat.CreateChatCompletionRequestMessage[]>([]);
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      prompt: ""
-    }
-  });
-
-  const isLoading = form.formState.isSubmitting;
-
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try {
-      const userMessage: OpenAI.Chat.CreateChatCompletionRequestMessage = { role: "user", content: values.prompt };
-      const newMessages = [...messages, userMessage];
-      
-      const response = await axios.post('/api/conversation', { messages: newMessages });
-      setMessages((current) => [...current, userMessage, response.data]);
-      
-      form.reset();
-    } catch (error: any) {
-      if (error?.response?.status === 403) {
-        //proModal.onOpen();
-        toast.error("Something went good.")
-      } else {
-        toast.error("Something went wrong.");
-      }
-    } finally {
-      router.refresh();
-    }
-    console.log(messages);
-  };
-
-  return (
-    <Form {...form}>
-      <form 
-        onSubmit={form.handleSubmit(onSubmit)} 
-        className="
-          rounded-lg 
-          border 
-          w-full 
-          p-4 
-          px-3 
-          md:px-6 
-          focus-within:shadow-sm
-          grid
-          grid-cols-12
-          gap-2
-        "
-      >
-        <FormField
-          name="prompt"
-          render={({ field }) => (
-            <FormItem className="col-span-12 lg:col-span-10">
-              <FormControl className="m-0 p-0">
-                <Input
-                  className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                  disabled={isLoading} 
-                  placeholder="How do I calculate the radius of a circle?" 
-                  {...field}
-                />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading} size="icon">
-          Generate
-        </Button>
-      </form>
-    </Form>
-  )
+    return (
+        <main className="flex items-center justify-center flex-col">
+            {/* <Navbar/>
+            <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+                <BackgroundGradientAnimation>
+                <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl ">
+                    Talk 2 <FlipWords words={words} />
+                </div>
+            </BackgroundGradientAnimation>
+            </section> */}
+            <Conversation/>
+        </main>
+    );
 }
 
-export default HomePage;
+export default Home
